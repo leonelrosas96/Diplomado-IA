bf6ba40c0c9a4f3f5daa98678477e73eaac230bf
leonelrosas96 <leonelrosas.rdz09@gmail.com>
2024-06-19 23:43:20 -0500

    Ejercicio_bucle_while

---
+++ Modulo_1/7_2Bucle_While.py
@@ -0,0 +1,10 @@
+numero_secreto = 3
+
+adivinado = False
+while (adivinado == False):
+    num = int(input('Introduzca un numero por favor: '))
+    if (num == numero_secreto):
+        print('Felicidades')
+        adivinado = True
+    else:
+        print("Intentalo de nuevo")
